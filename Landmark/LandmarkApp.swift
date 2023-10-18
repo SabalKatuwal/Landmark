27834794d826a7e9c08f71f04b571843a4eda7c9
SabalKatuwal <sabal.katuwal1@gmail.com>
2023-10-18 18:06:35 +0545

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Sabal on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
