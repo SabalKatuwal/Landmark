785a865aaf01da4ac09cc704e24d7b5f66a1ce36
SabalKatuwal <sabal.katuwal1@gmail.com>
2023-11-03 20:00:11 +0545

    Hike view progress

--- Landmark/Model/Hike.swift
+++ Landmark/Model/Hike.swift
@@ -7,28 +7,24 @@
 
 import Foundation
 
-struct Hike: Hashable, Identifiable, Codable {
-    
+struct Hike: Codable, Hashable, Identifiable {
     var id: Int
     var name: String
     var distance: Double
     var difficulty: Int
-    var observation: [Observation]
-    
+    var observations: [Observation]
+
     static var formatter = LengthFormatter()
     
-    var distanceText: String{
+    var distanceText: String {
         Hike.formatter
             .string(fromValue: distance, unit: .kilometer)
     }
-    
-    
+
     struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
         var elevation: Range<Double>
         var pace: Range<Double>
         var heartRate: Range<Double>
-        var distanceFromStart: Int
     }
-   
-    
 }
