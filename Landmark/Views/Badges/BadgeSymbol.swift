8e9650e464b4239a92e394f3da2f898ccd6f3a6e
SabalKatuwal <sabal.katuwal1@gmail.com>
2023-11-03 18:14:57 +0545

    modify badge symbol

--- Landmark/Views/Badges/BadgeSymbol.swift
+++ Landmark/Views/Badges/BadgeSymbol.swift
@@ -13,33 +13,66 @@ struct BadgeSymbol: View {
 
     var body: some View {
         GeometryReader { geometry in
-            Path { path in
+            ZStack {
+                
                 let width = min(geometry.size.width, geometry.size.height)
                 let height = width * 0.75
-                let spacing = width * 0.030
+                let spacing = width * 0.03
                 let middle = width * 0.5
                 let topWidth = width * 0.226
                 let topHeight = height * 0.488
-
-
-                path.addLines([
-                    CGPoint(x: middle, y: spacing),
-                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
-                    CGPoint(x: middle, y: topHeight / 2 + spacing),
-                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
-                    CGPoint(x: middle, y: spacing)
-                ])
                 
-                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
-                path.addLines([
-                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
-                    CGPoint(x: spacing, y: height - spacing),
-                    CGPoint(x: width - spacing, y: height - spacing),
-                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
-                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
-                ])
+                Path { path in
+                    
+
+
+                    path.addLines([
+                        CGPoint(x: middle, y: spacing),
+                        CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                        CGPoint(x: middle, y: topHeight / 2 + spacing),
+                        CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                        CGPoint(x: middle, y: spacing)
+                    ])
+                    
+                    
+                    
+                    path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                    
+                    path.addLines([
+                        CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                        CGPoint(x: spacing, y: height - spacing),
+                        CGPoint(x: width - spacing, y: height - spacing),
+                        CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                        CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                    ])
+                }
+                .fill(Self.symbolColor)
+                
+                //this two paths are added later and Z stack is created. These two paths given below is responsible for green adn blue small triangles.
+                Path { path in
+                    path.addLines([
+                        CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                        CGPoint(x: middle , y: topHeight + spacing),
+                        CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                        
+                        
+                    ])
+                }
+                .fill(.green)
+                
+                Path { path in
+                    path.addLines([
+                        
+                        CGPoint(x: middle , y: topHeight + spacing),
+                        CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                        CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                        
+                        
+                    ])
+                }
+                .fill(.blue)
+                
             }
-            .fill(Self.symbolColor)
         }
     }
 }
