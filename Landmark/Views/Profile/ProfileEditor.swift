c05a200daed2a6c91e78a07035cd2de5f15e68dc
SabalKatuwal <sabal.katuwal1@gmail.com>
2023-11-11 16:42:43 +0545

    ProfileEditor View with pickers and toggle bar

--- Landmark/Views/Profile/ProfileEditor.swift
+++ Landmark/Views/Profile/ProfileEditor.swift
@@ -9,6 +9,14 @@ import SwiftUI
 
 struct ProfileEditor: View {
     @Binding var profile: Profile
+    
+    //This code defines a computed property called dateRange that returns a ClosedRange of Date values. A ClosedRange is a type that represents a range of values that includes both the minimum and maximum values. In this case, the dateRange property represents the range of dates that is one year before and one year after the profile.goalDate property.
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate)!
+        return min...max
+    }
+    
     var body: some View {
        
         List {
@@ -17,6 +25,26 @@ struct ProfileEditor: View {
                     .bold()
                 Divider()
                 TextField("Username", text: $profile.username)
+            }
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notifications")
+                    .bold()
+            }
+            VStack(alignment: .leading, spacing: 20){
+                Text("Seasonal Photo").bold()
+                
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                DatePicker(selection: $profile.goalDate, in: dateRange, displayedComponents: .date) {
+                    Text("Goal Date").bold()
+                }
+                
             }
         }
          
